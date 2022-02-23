3b88e31c138d2274b96cd9547560f95328b0ae06
Joao Victor Rodrigues <jvsr@uchicago.edu>
2022-02-23 14:10:10 -0600

    first version

---
+++ xtract-magic.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import magic
+import pprint
+from itertools import chain
+
+
+def flatten(nested_list):
+    return list(chain.from_iterable(nested_list))
+
+
+xtract_magic = magic.Magic(
+    mime=True,
+    mime_encoding=True,
+    keep_going=True,
+    uncompress=True,
+    raw=True,
+    extension=True
+)
+
+
+def get_paths(dir_path):
+    return [os.path.join(dir_path, f) for f in os.listdir(dir_path)
+                  if os.path.isfile(os.path.join(dir_path, f))]
+
+
+def run_file(file_path=None):
+    if not file_path:
+        return
+    mdata = xtract_magic.from_file(file_path)
+    mdata = mdata.split(";")
+    for idx,m in enumerate(mdata):
+        mdata[idx] = m.split("\n-")
+        mdata[idx] = [n.strip() for n in mdata[idx]]
+        mdata[idx] = flatten([n.split(" ") for n in mdata[idx]])
+    return flatten(mdata)
+
+
+def run_dir(dir_path):
+    if not dir_path:
+        dir_path = str(os.getcwd())
+    file_paths = get_paths(dir_path)
+    bundled_metadata = dict()
+    for file_path in file_paths:
+        bundled_metadata[file_path] = run_file(file_path)
+    return bundled_metadata
+
+
+if __name__ == "__main__":
+    dir = sys.argv[1]
+    pprint.pp(run_dir(dir))
+    
